c1585fdff06bd3d45211127f07503e6221f535dc
Saurabh <saurabh8141@gmail.com>
2018-02-27 21:38:00 +0530

    Saurabh's Update

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var firebase = require('firebase');
 var fs = require('fs');
 var request = require('request');
-var util = require('util');
-var exec = require('child_process').exec;
+var amazon = require('amazon-product-api');
+//var util = require('util');
+//var exec = require('child_process').exec;
 //var firbaseui = require('firebaseui');
 var https = require('https');
+const OperationHelper = require('apac').OperationHelper;
 
 var app = express();
 
@@ -15,6 +17,13 @@ var options = {
     key : fs.readFileSync('key.pem'),
     cert : fs.readFileSync('cert.pem')
 };
+
+const opHelper = new OperationHelper({
+    awsId:     'AKIAJU4SDXGALNL72HXQ',
+    awsSecret: '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',
+    assocId:   ' buynearn00-21',
+    locale : 'IN'
+});
 
 var config = {
     apiKey: "AIzaSyCIvtWD0xa-VEknitACn-OJ-2pM7_GIUFs",
@@ -45,12 +54,30 @@ app.get('/login.html', function(req, res) {
     res.sendFile('login.html', {root : __dirname});
 });
 
-app.post('/index.html', function(req, res) {
+app.post('/amazon-api', function (req, res) {
+   
     var query = req.body['query'];
-    query = query.replace(/ /g, "+");
+    console.log(query);
+    opHelper.execute('ItemSearch', {
+      'SearchIndex': 'All',
+      'Keywords': query,
+      'ResponseGroup': 'ItemAttributes,Offers,Images'
+    }).then((response) => {
+        console.log("Req. recieved");
+        res.send(response.result);
+    }).catch((err) => {
+        //console.error("Something went wrong! ", err);
+    });
 
+});
+
+app.post('/flipkart-api', function(req, res) {
+    var query = req.body['query'];
+    console.log(query);
+    
+    query = query.replace(/ /g, "+");
     var options = {
-        url: "https://affiliate-api.flipkart.net/affiliate/search/json?query="+query+"&resultCount=5",
+        url: "https://affiliate-api.flipkart.net/affiliate/search/json?query="+query+"&resultCount=10",
         headers: {
             "Fk-Affiliate-Id" : "tenacious7",
             "Fk-Affiliate-Token" : "817aa29e4b4146aa8d00adc0f1b5ff9c"
@@ -59,10 +86,13 @@ app.post('/index.html', function(req, res) {
        
       request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          console.log(body);
+            //console.log(body);
+          res.send(body);
         }
       });
     
+
+
 });
 
 https.createServer(options, app).listen(3000, function() {
