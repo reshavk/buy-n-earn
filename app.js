e9a443bbb88c75e5161819d59e7f06da54b5d48f
Saurabh <saurabh8141@gmail.com>
2018-03-14 20:44:53 +0530

    Amazon and flipkart product showing correctly

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var firebase = require('firebase');
 var fs = require('fs');
 var request = require('request');
 var amazon = require('amazon-product-api');
+var flipkart = require('flipkart-affiliate');
 //var util = require('util');
 //var exec = require('child_process').exec;
 //var firbaseui = require('firebaseui');
@@ -18,6 +19,8 @@ var options = {
     cert : fs.readFileSync('cert.pem')
 };
 
+
+//For amazon-product-api
 const opHelper = new OperationHelper({
     awsId:     'AKIAJU4SDXGALNL72HXQ',
     awsSecret: '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',
@@ -25,12 +28,23 @@ const opHelper = new OperationHelper({
     locale : 'IN'
 });
 
+
+//For firebase api
 var config = {
     apiKey: "AIzaSyCIvtWD0xa-VEknitACn-OJ-2pM7_GIUFs",
     authDomain: "fir-20b6e.firebaseapp.com",
     databaseURL: "https://fir-20b6e.firebaseio.com",
     storageBucket: "fir-20b6e.appspot.com",
 };
+
+
+//For flipkart-affiliate-api
+var fkc = flipkart.createClient({
+  FkAffId: 'tenacious7', //as obtained from [Flipkart Affiliates API](https://affiliate.flipkart.com/api-docs/) 
+  FkAffToken: '817aa29e4b4146aa8d00adc0f1b5ff9c',
+  responseType: 'json'
+});
+
 firebase.initializeApp(config);
 
 app.use(bodyParser.urlencoded({ extended : false }));
@@ -56,6 +70,9 @@ app.get('/login.html', function(req, res) {
 
 app.post('/amazon-api', function (req, res) {
    
+
+    products = {};
+
     var query = req.body['query'];
     console.log(query);
     opHelper.execute('ItemSearch', {
@@ -63,35 +80,54 @@ app.post('/amazon-api', function (req, res) {
       'Keywords': query,
       'ResponseGroup': 'ItemAttributes,Offers,Images'
     }).then((response) => {
-        console.log("Req. recieved");
-        res.send(response.result);
-    }).catch((err) => {
-        //console.error("Something went wrong! ", err);
+        console.log("Response recieved from amazon");
+        products['amazon-result'] = response;
+        
+        //res.send(response.result);
+        console.log("Requesting from flipkart");
+
+        fkc.keywordSearch({
+        query: query, //search String 
+        resultCount: "5" //no of products in result 
+        }, function(err, results){
+            if(err){
+                console.log(err);
+            } 
+            else{
+                //console.log(results);
+                //res.send(results);
+                console.log("Response recieved from flipkart!!!");
+                products['flipkart-result'] = results;
+                res.send(products);
+            }
+
+
+        }).catch((err) => {
+            console.error("Something went wrong! ", err);
+        });
+
     });
 
 });
+
 
 app.post('/flipkart-api', function(req, res) {
     var query = req.body['query'];
     console.log(query);
     
-    query = query.replace(/ /g, "+");
-    var options = {
-        url: "https://affiliate-api.flipkart.net/affiliate/search/json?query="+query+"&resultCount=10",
-        headers: {
-            "Fk-Affiliate-Id" : "tenacious7",
-            "Fk-Affiliate-Token" : "817aa29e4b4146aa8d00adc0f1b5ff9c"
+    fkc.keywordSearch({
+        query: query, //search String 
+        resultCount: "5" //no of products in result 
+    }, function(err, results){
+        if(err){
+            console.log(err);
+        } else{
+            //console.log(results);
+            res.send(results);
         }
-      };
-       
-      request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            //console.log(body);
-          res.send(body);
-        }
-      });
-    
 
+
+    });
 
 });
 
